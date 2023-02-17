14063b00891b6697513a933b09fc6bb1eb0b3627
Ayush <ayushmultiverse@gmail.com>
2023-02-17 19:16:47 +0530

    Shop Menu1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Page1 from './Components/Page1/Page1';
 import Page2 from './Components/Page2/Page2';
 import Page3 from './Components/Page3/Page3';
+import ShopMenu from './Components/shopmenu/ShopMenu';
+
 function App() {
   return (
     <div className="App">
       {/* <Page1/> */}
-      <Page2/>
+      {/* <Page2 /> */}
       {/* <Page3/> */}
+      <ShopMenu />
     </div>
   );
 }
