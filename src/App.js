edbc1798f219ec2c9d521b89a54499eab5d1795a
RaunakAgarwal18 <raunak180803@gmail.com>
2023-02-18 22:02:42 +0530

    last 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Page1 from './Components/Page1/Page1';
 import Page2 from './Components/Page2/Page2';
 import Page3 from './Components/Page3/Page3';
+import Page4 from './Components/Page4/Page4';
+import Page6 from './Components/Page6/Page6';
+import Page5 from './Components/Page5/Page5';
 function App() {
   return (
     <div className="App">
       {/* <Page1/> */}
-      <Page2/>
+      {/* <Page2/> */}
       {/* <Page3/> */}
+      <Page4/>
+      {/* <Page5/> */}
+      {/* <Page6/> */}
     </div>
   );
 }
