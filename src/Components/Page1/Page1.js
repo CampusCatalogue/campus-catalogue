73cb7de09a9c9c62ba0cefca7894b01b9b33e9ca
Shubhradeep Das <shubhramail.das@gmail.com>
2023-02-19 03:58:37 +0530

    Back button added to login and signup page

--- src/Components/Page1/Page1.js
+++ src/Components/Page1/Page1.js
@@ -3,105 +3,123 @@ import React from "react";
 import styles from "./Style1.module.css";
 import img1 from "./Google.png";
 import img2 from "./Dots.png";
+import backbtn from "../../assets/images/shopmenuimg/backbtnicon.png";
 import { Link, Navigate } from "react-router-dom";
 import { auth, provider } from "../../firebase";
-import { signInWithPhoneNumber, signInWithPopup, RecaptchaVerifier } from "firebase/auth";
+import {
+    signInWithPhoneNumber,
+    signInWithPopup,
+    RecaptchaVerifier,
+} from "firebase/auth";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Page1() {
-	//  const handleClick=()=>{
-	//   console.log(" e tg")
-	//       return <Navigate to="/signupAuth"/>;
-	//   }
-	const [value, setValue] = useState("");
-	const [number, setNumber] = useState("");
+    //  const handleClick=()=>{
+    //   console.log(" e tg")
+    //       return <Navigate to="/signupAuth"/>;
+    //   }
+    const [value, setValue] = useState("");
+    const [number, setNumber] = useState("");
 
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	const handleClick = () => {
-		signInWithPopup(auth, provider).then((data) => {
-			setValue(data.user.email);
+    const handleClick = () => {
+        signInWithPopup(auth, provider).then((data) => {
+            setValue(data.user.email);
 
-			localStorage.setItem("email", data.user.email);
-			console.log(value);
-			navigate("/");
-		});
-	};
+            localStorage.setItem("email", data.user.email);
+            console.log(value);
+            navigate("/");
+        });
+    };
 
-	const signIn = (e) => {
-		e.preventDefault();
-		window.recaptchaVerifier = new RecaptchaVerifier(
-			"sign-in-button",
-			{
-				size: "invisible",
-				callback: (response) => {
-					// reCAPTCHA solved, allow signInWithPhoneNumber.
-					signInWithPhoneNumber(auth, number)
-						.then((data) => {
-							console.log(data);
-							navigate("/");
-						})
-						.error((error) => {
-							console.log(error);
-						});
-				},
-			},
-			auth
-		);
-	};
+    const signIn = (e) => {
+        e.preventDefault();
+        window.recaptchaVerifier = new RecaptchaVerifier(
+            "sign-in-button",
+            {
+                size: "invisible",
+                callback: (response) => {
+                    // reCAPTCHA solved, allow signInWithPhoneNumber.
+                    signInWithPhoneNumber(auth, number)
+                        .then((data) => {
+                            console.log(data);
+                            navigate("/");
+                        })
+                        .error((error) => {
+                            console.log(error);
+                        });
+                },
+            },
+            auth
+        );
+    };
 
-	useEffect(() => {
-		setValue(localStorage.getItem("email"));
-		console.log(value);
-	}, []);
-	return (
-		<>
-			<Link to="/">
-				<div className={styles.skip}>skip</div>
-			</Link>
-			<div>
-				<div className={styles.log}>Log in with</div>
-				<form onSubmit={signIn}>
-					<div className={styles.number}>
-						<div className={styles.nine}>+91</div>
-						<input
-							placeholder="Enter mobile number"
-							className={styles.input}
-							style={{
-								borderLeft: "none",
-								borderBottom: "none",
-								borderTop: "none",
-								borderRight: "none",
-							}}
-							value={number}
+    useEffect(() => {
+        setValue(localStorage.getItem("email"));
+        console.log(value);
+    }, []);
+    return (
+        <>
+            <Link to="/" style={{ height: "1vh" }}>
+                <div className={styles.back}>
+                    <img
+                        src={backbtn}
+                        alt="back"
+                        style={{
+                            width: "10vw",
+                            marginTop: "8vw",
+                            marginLeft: "3vw",
+                        }}
+                    ></img>
+                </div>
+                <div className={styles.skip}>skip</div>
+            </Link>
+            <div>
+                <div className={styles.log}>Log in with</div>
+                <form onSubmit={signIn}>
+                    <div className={styles.number}>
+                        <div className={styles.nine}>+91</div>
+                        <input
+                            placeholder="Enter mobile number"
+                            className={styles.input}
+                            style={{
+                                borderLeft: "none",
+                                borderBottom: "none",
+                                borderTop: "none",
+                                borderRight: "none",
+                            }}
+                            value={number}
                             onChange={(e) => setNumber(e.target.value)}
-						></input>
-					</div>
-					<div className={styles.continue} onClick={signIn}>
-						Continue
-					</div>
-				</form>
-				<div className={styles.or}>
-					<hr className={styles.hr} /> <span className={styles.ortext}>OR</span> <hr className={styles.hr} />
-				</div>
-				<div className={styles.icons}>
-					<button onClick={handleClick}>
-						{" "}
-						<img className={styles.img1} src={img1} />{" "}
-					</button>
+                        ></input>
+                    </div>
+                    <div className={styles.continue} onClick={signIn}>
+                        Continue
+                    </div>
+                </form>
+                <div className={styles.or}>
+                    <hr className={styles.hr} />{" "}
+                    <span className={styles.ortext}>OR</span>{" "}
+                    <hr className={styles.hr} />
+                </div>
+                <div className={styles.icons}>
+                    <button onClick={handleClick}>
+                        {" "}
+                        <img className={styles.img1} src={img1} />{" "}
+                    </button>
 
-					<img className={styles.img2} src={img2} />
-				</div>
-				<div className={styles.acc}>
-					If you dont have a account{" "}
-					<Link className={styles.sign} to="/signupAuth">
-						Sign up
-					</Link>
-					{/* <button  onClick={handleClick} ></button> */}
-				</div>
-				<div className={styles.term}>
-					{/* By continuing you agree to the{" "}
+                    <img className={styles.img2} src={img2} />
+                </div>
+                <div className={styles.acc}>
+                    If you dont have a account{" "}
+                    <Link className={styles.sign} to="/signupAuth">
+                        Sign up
+                    </Link>
+                    {/* <button  onClick={handleClick} ></button> */}
+                </div>
+                <div className={styles.term}>
+                    {/* By continuing you agree to the{" "}
           <span style={{ fontWeight: "bold", color: "black" }}>
             terms of service
           </span>{" "}
@@ -109,11 +127,13 @@ function Page1() {
           <span style={{ fontWeight: "bold", color: "black" }}>
             privacy policy
           </span> */}
-					By continuing you agree to the <b style={{ color: "#000" }}>Terms of service</b> and <strong style={{ color: "#000" }}>Privacy Policy</strong>
-				</div>
-			</div>
-		</>
-	);
+                    By continuing you agree to the{" "}
+                    <b style={{ color: "#000" }}>Terms of service</b> and{" "}
+                    <strong style={{ color: "#000" }}>Privacy Policy</strong>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Page1;
--- src/SignUp.js
+++ src/SignUp.js
@@ -5,37 +5,63 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
-
+import backbtn from "../src/assets/images/shopmenuimg/backbtnicon.png";
+import { Link } from "react-router-dom";
 const SignUp = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const navigate = useNavigate();
 
-	const signUp = (e) => {
-		e.preventDefault();
-		createUserWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				console.log(userCredential);
+    const signUp = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential);
                 navigate("/");
-
-			})
-			.catch((error) => {
-				console.log(error);
+            })
+            .catch((error) => {
+                console.log(error);
                 alert(error.message);
-			});
-	};
+            });
+    };
 
-	return (
-		<div className="sign-in-container">
-			<form onSubmit={signUp}>
-				<h1>Create Account</h1>
-				<input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-				<input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-				<button type="submit">Sign Up</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className="sign-in-container">
+            <Link to="/">
+                <img
+                    src={backbtn}
+                    style={{
+                        width: "10vw",
+                        marginTop: "8vw",
+                        marginLeft: "3vw",
+                    }}
+                    alt="back"
+                ></img>
+            </Link>
+            <form onSubmit={signUp} style={{ textAlign: "center" }}>
+                <h1 style={{ marginTop: "10vh", textAlign: "center" }}>
+                    Create Account
+                </h1>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    style={{ marginTop: "10vh", marginRight: "3vw" }}
+                ></input>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <p style={{ textAlign: "center", marginTop: "2vh" }}>
+                    <button type="submit">Sign Up</button>
+                </p>
+            </form>
+        </div>
+    );
 };
 
 export default SignUp;
