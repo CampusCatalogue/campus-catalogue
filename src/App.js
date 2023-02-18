61dc2daac56f0b278480ea069845c6b7c6445574
Shubhradeep Das <shubhramail.das@gmail.com>
2023-02-18 23:00:19 +0530

    Added Checkout Routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Page1 from './Components/Page1/Page1';
-import Page2 from './Components/Page2/Page2';
-import Page3 from './Components/Page3/Page3';
-import Page4 from './Components/Page4/Page4';
-import Page6 from './Components/Page6/Page6';
-import Page5 from './Components/Page5/Page5';
-function App() {
-  return (
-    <div className="App">
-      {/* <Page1/> */}
-      {/* <Page2/> */}
-      {/* <Page3/> */}
-      <Page4/>
-      {/* <Page5/> */}
-      {/* <Page6/> */}
-    </div>
-  );
-=======
-import logo from "./logo.svg";
-// import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 import Page1 from "./Components/Page1/Page1";
 import Page2 from "./Components/Page2/Page2";
 import Page3 from "./Components/Page3/Page3";
+import Page4 from "./Components/Page4/Page4";
+import Page6 from "./Components/Page6/Page6";
+import Page5 from "./Components/Page5/Page5";
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <Page1/> */}
+//       {/* <Page2/> */}
+//       {/* <Page3/> */}
+//       <Page4/>
+//       {/* <Page5/> */}
+//       {/* <Page6/> */}
+//     </div>
+//   );
+
+import logo from "./logo.svg";
+// import './App.css';
+// import Page1 from "./Components/Page1/Page1";
+// import Page2 from "./Components/Page2/Page2";
+// import Page3 from "./Components/Page3/Page3";
 import LandingPage from "./Components/LandingPage/landing_page";
 import ShopMenu from "./Components/shopmenu/ShopMenu";
-import { BrowserRouter as Router,Routes ,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Home from "./Home";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
@@ -34,40 +33,36 @@ import AuthDetails from "./AuthDetails";
 function App() {
     return (
         <div className="App">
+            {/* <Router>  */}
 
-             {/* <Router>  */}
-                 
-
-                    {/* <SignIn></SignIn>
+            {/* <SignIn></SignIn>
                     <SignUp></SignUp>
                     <AuthDetails></AuthDetails> */}
-                    {/* <Route path="/" element={SignIn}></Route>
+            {/* <Route path="/" element={SignIn}></Route>
                         <Route path="/phonesignup" element={PhoneSignUp}/> */}
 
-                         
-                 {/* <Route exact path="/signupAuth" component={SignUp} /> 
+            {/* <Route exact path="/signupAuth" component={SignUp} /> 
                  <Route exact path="/signinAuth" component={SignIn} />  */}
-                     <Router>
+            <Router>
                 <Routes>
                     <Route path="/login" element={<Page1 />} />
                     <Route path="/signup" element={<Page2 />} />
                     <Route path="/aboutyou" element={<Page3 />} />
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/ShopMenu" element={<ShopMenu />} />
+                    <Route path="/Confirm" element={<Page4 />} />
+                    <Route path="/Checkout" element={<Page5 />} />
+                    <Route path="/Payment" element={<Page6 />} />
 
                     {/* <Page2 /> */}
                     {/* <Page3 /> */}
                     {/* {<ShopMenu />} */}
                 </Routes>
             </Router>
-                
 
             {/* </Router> */}
-
         </div>
     );
->>>>>>> b926fb95f3157610d116767d9eee97da56a617bc
 }
-
 
 export default App;
